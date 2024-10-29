2f8218eab43326ad21b1492a2bfa12fb635ccef6
Hisham Thajudheen <hishamthajudheen@gmail.com>
2024-10-29 11:34:56 +0530

    Seperate apps added

--- healthcare_system/healthcare_system/hospital/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import PatientSignupView,PatientLoginView
-
-urlpatterns = [
-    path('signup/', PatientSignupView.as_view(), name='patient_signup'),
-     path('login/', PatientLoginView.as_view(), name='patient_login'),  # New login endpoint
-]
--- healthcare_system/healthcare_system/urls.py
+++
@@ -1,8 +0,0 @@
-# main project's urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),              # Admin interface
-    path('api/', include('healthcare_system.hospital.urls')),       # Include app URLs for the signup endpoint
-]
